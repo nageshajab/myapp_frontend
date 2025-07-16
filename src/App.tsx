1c906c8f42d837daebfd085d0c4641ff72507667
Nagesh Ajab <nageshajab@hotmail.com>
2025-07-16 16:49:04 +0530

    change password added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import ChangePassword from "./components/ChangePassword";
 import PasswordList from "./components/PasswordList";
 import PasswordForm from "./components/PasswordForm";
 import PasswordDetail from "./components/PasswordDetail";
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="home" element={<Home />} />
+          <Route path="changepassword" element={<ChangePassword />} />
           <Route path="passwordlist" element={<PasswordList />} />
           <Route path="passwordlist/create" element={<PasswordForm />} />
           <Route path="passwordlist/edit/:id" element={<PasswordForm />} />
--- src/api/passwordService.ts
+++ src/api/passwordService.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { API_URL } from "../../config";
 import { subscription_key } from "../../config";
 
-const API_BASE = API_URL;
 const api = axios.create({
       baseURL: API_URL,
       headers: {
@@ -10,8 +9,8 @@ const api = axios.create({
         "Ocp-Apim-Subscription-Key": subscription_key,
       },
 });
-export const createPassword = (data: any) => api.post(`${API_BASE}/PasswordCreate`, data);
-export const listPasswords = (data: any) => api.post(`${API_BASE}/GetPasswords`, data);
-export const getPassword = (id: string) => api.get(`${API_BASE}/PasswordGet?id=${id}`);
-export const updatePassword = ( data: any) => api.post(`${API_BASE}/PasswordUpdate`, data);
-export const deletePassword = (id: string) => api.post(`${API_BASE}/PasswordDelete?id=${id}`);
+export const createPassword = (data: any) => api.post(`/PasswordCreate`, data);
+export const listPasswords = (data: any) => api.post(`/GetPasswords`, data);
+export const getPassword = (id: string) => api.get(`/PasswordGet?id=${id}`);
+export const updatePassword = ( data: any) => api.post(`/PasswordUpdate`, data);
+export const deletePassword = (id: string) => api.post(`/PasswordDelete?id=${id}`);
