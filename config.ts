f8c6ab9fa2f43d8db4bf72d2160ee553b23f9666
Nagesh Ajab <nageshajab@hotmail.com>
2025-07-17 17:38:00 +0530

    1

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const API_URL = " http://localhost:7018/api";
-//export const API_URL = "https://myreactappapimanagementservice.azure-api.net/myreactappbackendapi"
+//export const API_URL = " http://localhost:7018/api";
+export const API_URL = "https://myreactappapimanagementservice.azure-api.net/myreactappbackendapi"
 export const subscription_key= "feb18fe559a248ccaa1ff22b56aaa3f2";
